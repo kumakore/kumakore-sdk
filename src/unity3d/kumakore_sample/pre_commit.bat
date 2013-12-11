9c231221191b61e4f575b9f63365d7960addb02f
chbfiv <chbfiv@gmail.com>
2013-12-11 11:32:43 -0800

    removed samples (invalid paths)

--- src/unity3d/kumakore_sample/pre_commit.bat
+++
@@ -1,8 +0,0 @@
-rd /s /q "$(ProjectDir)..\..\unity3d\kumakore_sample\Assets\com.kumakore\"
-xcopy /y "$(ProjectDir)*.cs" "$(ProjectDir)..\..\unity3d\kumakore_sample\Assets\com.kumakore\"
-xcopy /y "$(ProjectDir)fastJson\*.cs" "$(ProjectDir)..\..\unity3d\kumakore_sample\Assets\com.kumakore\fastJson\"
-
-
-rd /s /q "$(ProjectDir)..\..\unity3d\kumakore_sample\Assets\com.kumakore.test\"
-xcopy /y "$(ProjectDir)*.cs" "$(ProjectDir)..\..\unity3d\kumakore_sample\Assets\com.kumakore.test\"
-xcopy /y "$(ProjectDir)..\..\..\external\NUnit\nunit.framework.dll" "$(ProjectDir)..\..\unity3d\kumakore_sample\Assets\Plugins\"
