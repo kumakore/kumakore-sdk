8e09a13790f5d97ce8eda54fdd5b2397601f5594
chbfiv <chbfiv@gmail.com>
2014-03-16 09:28:45 -0700

    updated 0.7.0 docs

--- deliveries/v0.7.0.0/docs/unity3d/x86/html/search/all_69.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['ikumakoreinvokable',['IKumakoreInvokable',['../interfacecom_1_1kumakore_1_1_i_kumakore_invokable.html',1,'com::kumakore']]],
-  ['inventorymap',['InventoryMap',['../classcom_1_1kumakore_1_1_inventory_map.html',1,'com::kumakore']]],
-  ['isread',['isRead',['../group___a_c_t_i_o_n_s___m_a_t_c_h.html#ga69e7f324855b0e308f79bac74d553c73',1,'com::kumakore::ChatMessage']]],
-  ['issilhouette',['isSilhouette',['../group___a_c_t_i_o_n_s___u_s_e_r.html#ga4d5ee973a45835a9f1ed174f1f83a27d',1,'com::kumakore::FriendFacebook']]],
-  ['itembundle',['ItemBundle',['../classcom_1_1kumakore_1_1_item_bundle.html',1,'com::kumakore']]]
-];
